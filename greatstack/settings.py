27b47fc3076238837c823934d31745f528157575
Siva Kumar <sivakumarjeevavantham@gmail.com>
2025-03-21 16:57:39 +0530

    django authentication

--- greatstack/settings.py
+++ greatstack/settings.py
@@ -143,7 +143,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sivakumarjeevavantham@gmail.com'
-EMAIL_HOST_PASSWORD = 'cctr fsjg qpem kccd'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
