00fcc458a7578c36b9569f451b5ae746126c36a1
Siva Kumar <sivakumarjeevavantham@gmail.com>
2025-04-01 21:36:27 +0530

    dashboard editprofile modified4

--- accounts/views.py
+++ accounts/views.py
@@ -243,7 +243,7 @@ def my_orders(request):
 
 @login_required(login_url='login')
 def edit_profile(request):
-    userprofile = get_object_or_404(UserProfile, user=request.user)
+    # userprofile = get_object_or_404(UserProfile, user=request.user)
     try:
         userprofile = UserProfile.objects.get(user_id=request.user.id)
     except (UserProfile.DoesNotExist):
